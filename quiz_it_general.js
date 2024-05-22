72ee8275daa77b379b0e5c2aa0f1e02234c669b0
Piotr Strychaluk <pksmhz@wp.pl>
2024-05-22 02:30:44 +0200

    First 20 questions.

--- quiz_it_general.js
+++ quiz_it_general.js
@@ -1,125 +1,122 @@
     {
-        questionText: '(1) What is a source control?',
+        questionText: '(1) What is a version control software used for?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. To control revisions of source code files", isCorrect: true },
+            { id: 1, text: "B. To control the versions of applications installed", isCorrect: false },
+            { id: 2, text: "C. To check if given program version can be executed", isCorrect: false },
+            { id: 3, text: "D. To verify if all application are up to date", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(2) What can be stored in NoSQL database?',
+        questionText: '(2) What is the NoSQL database designed to store?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Key-value par", isCorrect: false },
+            { id: 1, text: "B. Structured data, typically in tables with rows and columns", isCorrect: false },
+            { id: 2, text: "C. Documents", isCorrect: false },
+            { id: 3, text: "D. Both A and C", isCorrect: true },
         ],
         answered: false
-    },    
+    },
     {
         questionText: '(3) What is a HTML?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. The computer programming language", isCorrect: false },
+            { id: 1, text: "B. The Javascript library to display dynamic content", isCorrect: false },
+            { id: 2, text: "C. The markup language for documents designed to be displayed in a web browser", isCorrect: true },
+            { id: 3, text: "D. Communication protocol", isCorrect: false },
         ],
         answered: false
     },
    	{
-        questionText: '(4) What is considered as script language?',
+        questionText: '(4) Which of the following can be used for scripting?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Perl", isCorrect: false },
+            { id: 1, text: "B. Python", isCorrect: false },
+            { id: 2, text: "C. HTML", isCorrect: false },
+            { id: 3, text: "D. Both A and B", isCorrect: true },
         ],
         answered: false
     },
     {
-        questionText: '(5) Wchich is not a valid Linux shell?',
+        questionText: '(5) Which of the following is not a valid Linux shell?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Bash", isCorrect: false },
+            { id: 1, text: "B. XML", isCorrect: true },
+            { id: 2, text: "C. Zsh", isCorrect: false },
+            { id: 3, text: "D. sh", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(6) What is a source control?',
+        questionText: '(6) Which of the following is not a version control software?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Git", isCorrect: false },
+            { id: 1, text: "B. CVS", isCorrect: false },
+            { id: 2, text: "C. JSON", isCorrect: true },
+            { id: 3, text: "D. Mercurial", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(7) What is not a computer programing language?',
+        questionText: '(7) Which of the following is not a computer programming language?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. C++", isCorrect: false },
+            { id: 1, text: "B. YAML", isCorrect: true },
+            { id: 2, text: "C. C#", isCorrect: false },
+            { id: 3, text: "D. Java", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(8) What LLM stands for?',
+        questionText: '(8) What LLM (form of AI) stands for?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Large Language Model", isCorrect: true },
+            { id: 1, text: "B. Low Level Machine", isCorrect: false },
+            { id: 2, text: "C. Long-Lived Model", isCorrect: false },
+            { id: 3, text: "D. Large Liquid Matrix ", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(9) What from below can not run containers?',
+        questionText: '(9) Which of the following cannot run containers?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Docker", isCorrect: false },
+            { id: 1, text: "B. Podman", isCorrect: false },
+            { id: 2, text: "C. JavaScript", isCorrect: true },
+            { id: 3, text: "D. Kubernetes", isCorrect: false },
         ],
         answered: false
     },
     {
         questionText: '(10) What Docker Compose can be used for?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. To create electronic music", isCorrect: false },
+            { id: 1, text: "B. To define and run multi-container applications", isCorrect: true },
+            { id: 2, text: "C. To create and maintain application properties", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(11) What from below can not run containers?',
+        questionText: '(11) Docker is a software for:',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Create plugins for your application", isCorrect: false },
+            { id: 1, text: "B. OS-level virtualization", isCorrect: true },
+            { id: 2, text: "C. JavaScript layout library ", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(12) What from below can not run containers?',
+        questionText: '(12) Which technology is used in cryptocurrency transactions?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Distributed computing", isCorrect: false },
+            { id: 1, text: "B. Blockchain", isCorrect: true },
+            { id: 2, text: "C. Protobuf", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(13) What from below is not an computer operating system?',
+        questionText: '(13) Which of the following list is not an computer operating system?',
         options: [
             { id: 0, text: "A. Solaris", isCorrect: false },
             { id: 1, text: "B. MAC OS", isCorrect: false },
@@ -129,20 +126,20 @@
         answered: false
     },
     {
-        questionText: '(14) What is load balancer?',
+        questionText: '(14) To improve availability and resilience of your web application, you would use:',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Load balancer", isCorrect: false },
+            { id: 1, text: "B. Reverse proxy", isCorrect: false },
+            { id: 2, text: "C. Application gateway", isCorrect: false },
+            { id: 3, text: "D. All the above answers are correct", isCorrect: true },
         ],
         answered: false
     },
     {
-        questionText: '(15) What can be used to extend existing software?',
+        questionText: '(15) What can be used to extend already existing software??',
         options: [
-            { id: 0, text: "A. Plugin", isCorrect: true },
-            { id: 1, text: "B. WSDL", isCorrect: false },
+            { id: 0, text: "A. Plugin", isCorrect: false },
+            { id: 1, text: "B. Property file", isCorrect: false },
             { id: 2, text: "C. Extension", isCorrect: false },
             { id: 3, text: "D. Both A and C", isCorrect: true },
         ],
@@ -153,23 +150,22 @@
         options: [
             { id: 0, text: "A. Apache", isCorrect: false },
             { id: 1, text: "B. Derby", isCorrect: true },
-            { id: 2, text: "C. NGIX", isCorrect: false },
-            { id: 3, text: "D. Publicfile", isCorrect: false },
+            { id: 2, text: "C. nginx", isCorrect: false },
+            { id: 3, text: "D. Microsoft Internet Information Services", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(17) What is web assembly?',
+        questionText: '(17) What is WebAssembly?',
         options: [
-            { id: 0, text: "A. Virtual DOM", isCorrect: true },
-            { id: 1, text: "B. Shadow Dom", isCorrect: false },
-            { id: 2, text: "C. Original DOM", isCorrect: false },
-            { id: 3, text: "D. Both A and B", isCorrect: false },
+            { id: 0, text: "A. Programming language that can be writen in web browser", isCorrect: false },
+            { id: 1, text: "B. Portable binary-code format to enable high-performance applications on web pages", isCorrect: true },
+            { id: 2, text: "C. Module bundler for JavaScript", isCorrect: false },
         ],
         answered: false
     },
     {
-        questionText: '(18) Creating programs using is "closest to the hardware"?',
+        questionText: '(18) To create a program that is "closest to the hardware", you would use:',
         options: [
             { id: 0, text: "A. C++", isCorrect: false },
             { id: 1, text: "B. Assembly language", isCorrect: true },
@@ -181,15 +177,15 @@
     {
         questionText: '(19) What language cannot be run in modern browser natively?',
         options: [
-            { id: 0, text: "A. Typescript", isCorrect: true },
-            { id: 1, text: "B. Javascript", isCorrect: false },
+            { id: 0, text: "A. TypeScript", isCorrect: false },
+            { id: 1, text: "B. JavaScript", isCorrect: false },
             { id: 2, text: "C. Java", isCorrect: false },
-            { id: 3, text: "D. Both A and C", isCorrect: false },
+            { id: 3, text: "D. Both A and C", isCorrect: true },
         ],
         answered: false
     },
     {
-        questionText: '(20) Which of the following cannot be used to comunicate between programs?',
+        questionText: '(20) Which of the following cannot be used to communicate between two different programs?',
         options: [
             { id: 0, text: "A. REST", isCorrect: false },
             { id: 1, text: "B. SOAP", isCorrect: false },
