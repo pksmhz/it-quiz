474be30cab61e89417e4ae6d4db300de07438fa6
Piotr Strychaluk <pksmhz@wp.pl>
2024-05-23 22:17:36 +0200

    Next 30 questions.

--- quiz_it_general.js
+++ quiz_it_general.js
@@ -131,7 +131,7 @@
             { id: 0, text: "A. Load balancer", isCorrect: false },
             { id: 1, text: "B. Reverse proxy", isCorrect: false },
             { id: 2, text: "C. Application gateway", isCorrect: false },
-            { id: 3, text: "D. All the above answers are correct", isCorrect: true },
+            { id: 3, text: "D. All of the above answers are correct", isCorrect: true },
         ],
         answered: false
     },
@@ -194,3 +194,289 @@
         ],
         answered: false
     },
+    {
+        questionText: '(21) What is a difference between little and big endian architecture?',
+        options: [
+            { id: 0, text: "A. Number of bits. One is 32-bit, other 64-bit ", isCorrect: false },
+            { id: 1, text: "B. First one is based on x86, second on ARM", isCorrect: false },
+            { id: 2, text: "C. Position of the most significant byte", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(22) How many bits are in one byte?',
+        options: [
+            { id: 0, text: "A. 4 bits", isCorrect: false },
+            { id: 1, text: "B. 8 bits", isCorrect: true },
+            { id: 2, text: "C. 16 bits", isCorrect: false },
+            { id: 3, text: "C. 32 bits", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(23) What is serialization in computing?',
+        options: [
+            { id: 0, text: "A. Executing I/O operations in queue", isCorrect: false },
+            { id: 1, text: "B. Process of translating a data structure or object state into a format that can be stored or transmitted", isCorrect: true },
+            { id: 2, text: "C. Displaying data in rows and columns", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(24) How many bits are in one byte?',
+        options: [
+            { id: 0, text: "A. 4 bits", isCorrect: false },
+            { id: 1, text: "B. 8 bits", isCorrect: true },
+            { id: 2, text: "C. 16 bits", isCorrect: false },
+            { id: 3, text: "D. 32 bits", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(25) What is serialization in computing?',
+        options: [
+            { id: 0, text: "A. Executing I/O operations in queue", isCorrect: false },
+            { id: 1, text: "B. Process of translating a data structure or object state into a format that can be stored or transmitted", isCorrect: true },
+            { id: 2, text: "C. Displaying data in rows and columns", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(26) What is a message broker in computing?',
+        options: [
+            { id: 0, text: "A. Tool to analyze database data", isCorrect: false },
+            { id: 1, text: "B. Software to facilitate the debugging of incoming messages", isCorrect: false },
+            { id: 2, text: "C. An intermediary computer program that translates a message from the protocol of the sender to the protocol of the receiver", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(27) Which computer programming language is using its own software environment (virtual machine) to run?',
+        options: [
+            { id: 0, text: "A. Java", isCorrect: false },
+            { id: 1, text: "B. C++", isCorrect: false },
+            { id: 2, text: "C. C#", isCorrect: false },
+            { id: 3, text: "D. Both A and C", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(28) Some programming languages are using its own software environment (virtual machine) to run.  What is the main advantage of it?',
+        options: [
+            { id: 0, text: "A. Direct access to hardware", isCorrect: false },
+            { id: 1, text: "B. Portability", isCorrect: true },
+            { id: 2, text: "C. Execution speed", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(29) Which of the following list is not a communication protocol?',
+        options: [
+            { id: 0, text: "A. TCP/IP", isCorrect: false },
+            { id: 1, text: "B. SMTP", isCorrect: false },
+            { id: 2, text: "C. HTML", isCorrect: true },
+            { id: 3, text: "D. FTP", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(30) What information can be included in byte-order mark (BOM) character at the start of text stream',
+        options: [
+            { id: 0, text: "A. Endianness of the text stream", isCorrect: false },
+            { id: 1, text: "B. If text stream is encoded in Unicode", isCorrect: false },
+            { id: 2, text: "C. Which Unicode character encoding is used", isCorrect: false },
+            { id: 3, text: "D. All of the above answers are correct", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(31) Which of the following list is not a character encoding?',
+        options: [
+            { id: 0, text: "A. UTF-8", isCorrect: false },
+            { id: 1, text: "B. US-ASCII", isCorrect: false },
+            { id: 2, text: "C. CCS-16", isCorrect: true },
+            { id: 3, text: "D. UTF-32", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(32) Which of the following list is not a character encoding?',
+        options: [
+            { id: 0, text: "A. UTF-8", isCorrect: false },
+            { id: 1, text: "B. US-ASCII", isCorrect: false },
+            { id: 2, text: "C. CCS-16", isCorrect: true },
+            { id: 3, text: "D. UTF-32", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(33) Which hashing algorithm you should avoid?',
+        options: [
+            { id: 0, text: "A. MD5", isCorrect: false },
+            { id: 1, text: "B. SHA-1", isCorrect: false },
+            { id: 2, text: "C. SHA-256", isCorrect: false },
+            { id: 3, text: "D. Both A and B", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(34) Web Services Description Language (WSDL) is used typically with',
+        options: [
+            { id: 0, text: "A. ODBC", isCorrect: false },
+            { id: 1, text: "B. SOAP", isCorrect: false },
+            { id: 2, text: "C. Kafka", isCorrect: false },
+            { id: 3, text: "D. RMI", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(35) Which of the following list is not a HTTP request method?',
+        options: [
+            { id: 0, text: "A. GET", isCorrect: false },
+            { id: 1, text: "B. FETCH", isCorrect: true },
+            { id: 2, text: "C. PATCH", isCorrect: false },
+            { id: 3, text: "D. OPTIONS", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(36) What does it mean that a request method is idempotent?',
+        options: [
+            { id: 0, text: "A. Method can be executed more than once", isCorrect: false },
+            { id: 1, text: "B. It is Read only method", isCorrect: false },
+            { id: 2, text: "C. Multiple identical requests with that method have the same effect as a single such request", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(37) What does the 403 HTTP status code mean?',
+        options: [
+            { id: 0, text: "A. Forbidden - User not having the necessary permissions or attempting a prohibited action", isCorrect: true },
+            { id: 1, text: "B. Unauthorized - Authentication is required and has failed or has not yet been provided", isCorrect: false },
+            { id: 2, text: "C. Not Found - The requested resource could not be found", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(38) What does the 500 HTTP status code mean?',
+        options: [
+            { id: 0, text: "A. Upgrade Required - The client should switch to a different protocol ", isCorrect: false },
+            { id: 1, text: "B. Internal Server Error - A generic error message, given when an unexpected condition was encountered", isCorrect: true },
+            { id: 2, text: "C. Not Found - The requested resource could not be found", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(39) What does the 200 HTTP status code mean?',
+        options: [
+            { id: 0, text: "A. Upgrade Required - The client should switch to a different protocol ", isCorrect: false },
+            { id: 1, text: "B. Permanent Redirect - This and all future requests should be directed to the given URI", isCorrect: false },
+            { id: 2, text: "C. OK - Response for successful HTTP request", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(40) In computer programming, CRUD consists of the four basic operations of persistent storage. Which operation is not a part of it?',
+        options: [
+            { id: 0, text: "A. Create", isCorrect: false },
+            { id: 1, text: "B. Read", isCorrect: false },
+            { id: 2, text: "C. Upload", isCorrect: true },
+            { id: 3, text: "C. Delete", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(41) What is an HTTPS protocol?',
+        options: [
+            { id: 0, text: "A. It is an extension of the Hypertext Transfer Protocol (HTTP). It uses encryption for secure communication", isCorrect: true },
+            { id: 1, text: "B. It is database access layer", isCorrect: false },
+            { id: 2, text: "C. It is a protocol for managing remote Docker containers", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(42) What is a hash function?',
+        options: [
+            { id: 0, text: "A. An algorithm for performing encryption or decryption", isCorrect: false },
+            { id: 1, text: "B. An algorithm that perform a one-way conversion of an arbitrary number of bytes of data into a byte array of a fixed size", isCorrect: true },
+            { id: 2, text: "C. An algorithm for creating Universally Unique Identifier (UUID)", isCorrect: false }
+        ],
+        answered: false
+    },
+    {
+        questionText: '(43) What is a GraphQL?',
+        options: [
+            { id: 0, text: "A. An JavaScript visualization library", isCorrect: false },
+            { id: 1, text: "B. An utility to display connections between servers", isCorrect: false },
+            { id: 2, text: "C. An open-source data query and manipulation language for APIs", isCorrect: true }
+        ],
+        answered: false
+    },
+    {
+        questionText: '(44) To edit a text file, you can use',
+        options: [
+            { id: 0, text: "A. Vim", isCorrect: false },
+            { id: 1, text: "B. GNU Emacs", isCorrect: false },
+            { id: 2, text: "C. Nano text editor", isCorrect: false },
+            { id: 3, text: "D. All of the above answers are correct", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(45) Which of the following list is not a protocol related to electronic mail?',
+        options: [
+            { id: 0, text: "A. SMTP", isCorrect: false },
+            { id: 1, text: "B. POP3", isCorrect: false },
+            { id: 2, text: "C. SNMP", isCorrect: true },
+            { id: 3, text: "D. IMAP", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(46) Which of the following list is not a protocol related to electronic mail?',
+        options: [
+            { id: 0, text: "A. SMTP", isCorrect: false },
+            { id: 1, text: "B. POP3", isCorrect: false },
+            { id: 2, text: "C. SNMP", isCorrect: true },
+            { id: 3, text: "D. IMAP", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(47) To identify allowed request methods for given endpoint, you would',
+        options: [
+            { id: 0, text: "A. use HTTP PATCH method", isCorrect: false },
+            { id: 1, text: "B. verify endpoint certificate", isCorrect: false },
+            { id: 2, text: "C. use HTTP OPTIONS method", isCorrect: true },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(48) Regarding HTTP methods, which of the following is false',
+        options: [
+            { id: 0, text: "A. PATCH request method applies partial modifications to a resource.", isCorrect: false },
+            { id: 1, text: "B. PUT request method creates a new resource or replaces a representation of the target resource with the request payload.", isCorrect: false },
+            { id: 2, text: "C. POST is idempotent: calling it once or several times successively has the same effect", isCorrect: true },
+            { id: 3, text: "D. GET should not have meaningful payload by convention", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(49) To delete specified resource via REST API, you would use',
+        options: [
+            { id: 0, text: "A. HTTP PATCH request method", isCorrect: false },
+            { id: 1, text: "B. HTTP DELETE request method", isCorrect: true },
+            { id: 2, text: "C. HTTP REMOVE request method", isCorrect: false },
+        ],
+        answered: false
+    },
+    {
+        questionText: '(50) Which of the following is not a valid XML?',
+        options: [
+            { id: 0, text: "A. <root><p>&value</p></root>", isCorrect: true },
+            { id: 1, text: "B. <root><p><![CDATA[value<0]]></p></root>", isCorrect: false },
+            { id: 2, text: "C. <root><p>value;</p></root>", isCorrect: false },
+        ],
+        answered: false
+    },
+
